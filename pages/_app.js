b6c495605cc24b6e2a56e8cbe88cefd35ce84a1c
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-04 08:49:18 +0300

    tailwind Has Been Integrated

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css'
+
+// import '../styles/globals.css'
+
+function StudentInformationSystem({Component, pageProps}) {
+    return <Component {...pageProps} />
+}
+
+export default StudentInformationSystem
--- pages/login/index.js
+++ pages/login/index.js
@@ -11,34 +11,26 @@ export default function Main() {
                 <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                     <div className="inline-flex rounded-md shadow">
                         <a
-                            href="/dashboard/student"
+                            href="/login/student"
                             className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                         >
-                            Get started
+                            Öğrenci Girişi
                         </a>
                     </div>
                     <div className="ml-3 inline-flex rounded-md shadow">
                         <a
-                            href="#"
+                            href="/login/teacher"
                             className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                         >
-                            Learn more
+                            Öğretmen Girişi
                         </a>
                     </div>
                     <div className="ml-3 inline-flex rounded-md shadow">
                         <a
-                            href="#"
-                            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
+                            href="/login/officer"
+                            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                         >
-                            Learn more
-                        </a>
-                    </div>
-                    <div className="ml-3 mr-72 inline-flex rounded-md shadow">
-                        <a
-                            href="#"
-                            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
-                        >
-                            Learn more
+                            Personel Girişi
                         </a>
                     </div>
                 </div>
