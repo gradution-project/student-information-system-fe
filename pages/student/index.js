ef58968bc8b9eb437aa2fc4c11a6e1131e72e75f
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-10 23:30:07 +0300

    Student Academic and Personal Info Screens Have Been Fixed

---
+++ pages/student/index.js
@@ -0,0 +1,11 @@
+import StudentNavbar from "../../public/components/navbar/student/student-navbar";
+import SISTitle from "../../public/components/page-titles";
+
+export default function StudentDashboard() {
+    return (
+        <div>
+            <SISTitle/>
+            <StudentNavbar/>
+        </div>
+    )
+}
