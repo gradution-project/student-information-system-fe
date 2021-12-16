a31070b9624fb03eacea4dad2ad0f5af30491d9e
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-16 15:39:29 +0300

    Student Login Screen Has Been Fixed

--- pages/login/student/index.js
+++ pages/login/student/index.js
@@ -47,7 +47,7 @@ export default function StudentLogin() {
 
         event.preventDefault();
 
-        const res = await fetch("https://sis-be.herokuapp.com/login/student", {
+        const loginRes = await fetch("https://sis-be.herokuapp.com/login/student", {
             body: JSON.stringify({studentId: studentNumber, password: password}),
             headers: {'Content-Type': 'application/json'},
             method: 'POST'
