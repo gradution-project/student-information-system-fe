e1e173612f6ef97b09d152609dd187a30fbff841
Berrin Korkutata <berrin212168@hotmail.com>
2022-02-14 17:18:08 +0300

    Done Faculty List page and Connection Api's Request

--- pages/officer/operation/faculty/index.js
+++ pages/officer/operation/faculty/index.js
@@ -1,39 +1,111 @@
 import SISTitle from "../../../../public/components/page-titles";
 import OfficerNavbar from "../../../../public/components/navbar/officer/officer-navbar";
+import {useRouter} from "next/router";
+import {facultyStatuses} from "../../../../public/constants/faculty";
 
+export async function getServerSideProps() {
+    const facultyResponse = await fetch("http://localhost:8585/faculty?status=ALL", {
+        headers: {'Content-Type': 'application/json'},
+        method: 'GET'
+    });
+    const facultiesData = await facultyResponse.json();
+    if (facultiesData.success) {
+        return {
+            props: {faculties: facultiesData.response}
+        }
+    }
+}
 
-export default function Faculty() {
+export default function FacultyList({faculties}) {
+
+    const router = useRouter();
+
+    const pushSavePage = async (event) => {
+        event.preventDefault();
+        await router.push('/officer/operation/faculty/save');
+    }
+
     return (
         <div>
             <SISTitle/>
             <OfficerNavbar/>
-            <form className="px-4 py-12 max-w-2xl mx-auto space-y-6">
-                <div className="py-2 px-4 shadow overflow-hidden sm:rounded-md">
-                    <div className="sm:col-span-6">
-                        <label htmlFor="teacher-number"
-                               className="ml-0.5 text-xl text-sis-darkblue font-phenomenaBold">
-                            FAKÜLTE İSMİ
-                        </label>
-                        <input
-                            type="text"
-                            name="teacher-number"
-                            id="teacher-number"
-                            value=""
-                            className="font-phenomenaRegular text-gray-400 mt-1 focus:ring-sis-yellow focus:border-sis-yellow block w-full shadow-sm sm:text-xl border-gray-300 rounded-md"
-                        />
-                    </div>
-
-                    <div className="px-4 py-3 text-right sm:px-6">
-                        <button
-                            type="submit"
-                            className="font-phenomenaBold py-2 px-4 border border-transparent shadow-sm text-xl rounded-md text-white bg-sis-yellow hover:bg-sis-darkblue"
-                        >
-                            KAYDET
-                        </button>
-                    </div>
+            <div className="select-none px-28 py-5 mx-auto space-y-6">
+                <div className="px-12 py-10 text-left bg-gray-50 rounded-2xl shadow-xl">
+                    <a className="font-phenomenaExtraBold text-left text-4xl text-sis-darkblue">
+                        FAKÜLTE LİSTESİ
+                    </a>
+                    <button
+                        type="submit"
+                        onClick={pushSavePage}
+                        className="font-phenomenaBold float-right py-2 px-4 border border-transparent shadow-sm text-xl rounded-md text-white bg-sis-success hover:bg-green-600"
+                    >
+                        FAKÜLTE EKLE
+                    </button>
                 </div>
-            </form>
+                {(
+                    faculties != null
+                        ?
+                        <div className="flex flex-col">
+                            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                    <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                        <table className="bg-gray-50 min-w-full divide-y divide-gray-200">
+                                            <thead className="font-phenomenaBold text-xl text-gray-500 text-left">
+                                            <tr>
+                                                <th
+                                                    scope="col"
+                                                    className="select-none px-6 py-3 tracking-wider"
+                                                >
+                                                    FAKÜLTE ADI
+                                                </th>
+                                                <th
+                                                    scope="col"
+                                                    className="select-none px-6 py-3 tracking-wider"
+                                                >
+                                                    STATÜSÜ
+                                                </th>
+                                            </tr>
+                                            </thead>
+                                            <tbody className="bg-white divide-y divide-gray-200">
+                                            {faculties.map((faculty) => (
+                                                <tr key={faculty.facultyId}>
+                                                    <td className="px-2 py-4 whitespace-nowrap">
+                                                        <div className="flex items-center">
+                                                            <div className="ml-4">
+                                                                <div
+                                                                    className="font-phenomenaBold text-xl text-sis-darkblue">{faculty.name}</div>
+                                                                <div
+                                                                    className="font-phenomenaRegular text-lg text-gray-500">{faculty.facultyId}</div>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td className="px-6 py-4 whitespace-nowrap">
+                                                        {facultyStatuses.map((fStatus) => (
+                                                            faculty.status === fStatus.enum
+                                                                ?
+                                                                fStatus.miniComponent
+                                                                :
+                                                                null
+                                                        ))}
+                                                    </td>
+                                                    <td className="ml-10 px-6 py-4 text-right font-phenomenaBold text-xl">
+                                                        <a href={'/officer/operation/faculty/information/detail/' + faculty.facultyId}
+                                                           className='text-sis-yellow'>
+                                                            DETAY
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        null
+                )}
+            </div>
         </div>
-
     )
 }
