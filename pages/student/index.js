aedc82326beb98a085dd99c631be4d60c4519052
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-26 09:27:04 +0300

    Authorization Check Has Been Added to StudentDashboard Page

--- pages/student/index.js
+++ pages/student/index.js
@@ -1,25 +1,51 @@
 import StudentNavbar from "../../public/components/navbar/student/student-navbar";
 import SISTitle from "../../public/components/page-titles";
 import Cookies from "universal-cookie";
+import UnauthorizedAccessPage from "../401";
 
-export default function StudentDashboard() {
+export async function getServerSideProps(context) {
+    const studentId = context.req.cookies['studentNumber']
+    if (studentId === undefined) {
+        return {
+            props: {
+                isPagePermissionSuccess: false
+            }
+        }
+    }
+
+    return {
+        props: {
+            isPagePermissionSuccess: true
+        }
+    }
+}
+
+export default function StudentDashboard({isPagePermissionSuccess}) {
 
     const cookies = new Cookies();
 
     return (
-        <div>
-            <SISTitle/>
-            <StudentNavbar/>
-            <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
-                <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
+        <>
+            {(
+                isPagePermissionSuccess === true
+                    ?
                     <div>
-                        Merhaba <a className="font-phenomenaBold">{cookies.get("studentFullName")}</a>,
+                        <SISTitle/>
+                        <StudentNavbar/>
+                        <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
+                            <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
+                                <div>
+                                    Merhaba <a className="font-phenomenaBold">{cookies.get("studentFullName")}</a>,
+                                </div>
+                                <div>
+                                    Öğrenci Bilgi Sistemine Hoşgeldiniz!
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        Öğrenci Bilgi Sistemine Hoşgeldiniz!
-                    </div>
-                </div>
-            </div>
-        </div>
+                    :
+                    <UnauthorizedAccessPage/>
+            )}
+        </>
     )
 }
