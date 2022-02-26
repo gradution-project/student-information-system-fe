ce6f020f75d4f992f24aca9f4269ad340bdbda23
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-26 19:49:07 +0300

    Authorization Check and Get Local Storage Methods Have Been Added to Officer Home Page

--- pages/officer/index.js
+++ pages/officer/index.js
@@ -1,10 +1,10 @@
 import SISTitle from "../../public/components/page-titles";
 import OfficerNavbar from "../../public/components/navbar/officer/officer-navbar";
-import Cookies from "universal-cookie";
 import UnauthorizedAccessPage from "../401";
+import {getOfficerFullName, getOfficerNumberWithContext} from "../../public/storage/officer";
 
 export async function getServerSideProps(context) {
-    const officerId = context.req.cookies['officerNumber']
+    const officerId = getOfficerNumberWithContext(context);
     if (officerId === undefined) {
         return {
             props: {
@@ -22,28 +22,26 @@ export async function getServerSideProps(context) {
 
 export default function OfficerDashboard({isPagePermissionSuccess}) {
 
-    if (isPagePermissionSuccess) {
-        const cookies = new Cookies();
-
-        return (
-            <div>
-                <SISTitle/>
-                <OfficerNavbar/>
-                <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
-                    <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
-                        <div>
-                            Merhaba <a className="font-phenomenaBold">{cookies.get("officerFullName")}</a>,
-                        </div>
-                        <div>
-                            Öğrenci Bilgi Sistemine Hoşgeldiniz!
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
+    if (!isPagePermissionSuccess) {
         return (
             <UnauthorizedAccessPage user=""/>
         )
     }
+
+    return (
+        <div>
+            <SISTitle/>
+            <OfficerNavbar/>
+            <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
+                <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
+                    <div>
+                        Merhaba <a className="font-phenomenaBold">{getOfficerFullName()}</a>,
+                    </div>
+                    <div>
+                        Öğrenci Bilgi Sistemine Hoşgeldiniz!
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
