9b0391f68709fecc8d2044f8aaabbd141737cdbd
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-26 10:07:29 +0300

    Authorization Check Has Been Added to OfficerDashboard Page

--- pages/officer/index.js
+++ pages/officer/index.js
@@ -1,25 +1,49 @@
 import SISTitle from "../../public/components/page-titles";
 import OfficerNavbar from "../../public/components/navbar/officer/officer-navbar";
 import Cookies from "universal-cookie";
+import UnauthorizedAccessPage from "../401";
 
-export default function OfficerDashboard() {
+export async function getServerSideProps(context) {
+    const officerId = context.req.cookies['officerNumber']
+    if (officerId === undefined) {
+        return {
+            props: {
+                isPagePermissionSuccess: false
+            }
+        }
+    }
 
-    const cookies = new Cookies();
+    return {
+        props: {
+            isPagePermissionSuccess: true
+        }
+    }
+}
 
-    return (
-        <div>
-            <SISTitle/>
-            <OfficerNavbar/>
-            <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
-                <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
-                    <div>
-                        Merhaba <a className="font-phenomenaBold">{cookies.get("officerFullName")}</a>,
-                    </div>
-                    <div>
-                        Öğrenci Bilgi Sistemine Hoşgeldiniz!
+export default function OfficerDashboard({isPagePermissionSuccess}) {
+
+    if (isPagePermissionSuccess) {
+        const cookies = new Cookies();
+
+        return (
+            <div>
+                <SISTitle/>
+                <OfficerNavbar/>
+                <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
+                    <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
+                        <div>
+                            Merhaba <a className="font-phenomenaBold">{cookies.get("officerFullName")}</a>,
+                        </div>
+                        <div>
+                            Öğrenci Bilgi Sistemine Hoşgeldiniz!
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <UnauthorizedAccessPage user=""/>
+        )
+    }
 }
