755365f52c63f9f5955da4812be4301e0fc34231
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-26 09:35:45 +0300

    Authorization Check Has Been Updated in StudentDashboard Page

--- pages/student/index.js
+++ pages/student/index.js
@@ -44,7 +44,7 @@ export default function StudentDashboard({isPagePermissionSuccess}) {
                         </div>
                     </div>
                     :
-                    <UnauthorizedAccessPage/>
+                    <UnauthorizedAccessPage user=""/>
             )}
         </>
     )
