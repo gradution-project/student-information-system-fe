12b95473a9b7157169b32311c2cee418a88759b1
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-02-26 19:48:07 +0300

    Authorization Check and Get Local Storage Methods Have Been Added to Student Home Page

--- pages/student/index.js
+++ pages/student/index.js
@@ -1,10 +1,10 @@
 import StudentNavbar from "../../public/components/navbar/student/student-navbar";
 import SISTitle from "../../public/components/page-titles";
-import Cookies from "universal-cookie";
 import UnauthorizedAccessPage from "../401";
+import {getStudentFullName, getStudentNumberWithContext} from "../../public/storage/student";
 
 export async function getServerSideProps(context) {
-    const studentId = context.req.cookies['studentNumber']
+    const studentId = getStudentNumberWithContext(context)
     if (studentId === undefined) {
         return {
             props: {
@@ -22,29 +22,26 @@ export async function getServerSideProps(context) {
 
 export default function StudentDashboard({isPagePermissionSuccess}) {
 
-    if (isPagePermissionSuccess) {
-
-        const cookies = new Cookies();
-
-        return (
-            <div>
-                <SISTitle/>
-                <StudentNavbar/>
-                <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
-                    <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
-                        <div>
-                            Merhaba <a className="font-phenomenaBold">{cookies.get("studentFullName")}</a>,
-                        </div>
-                        <div>
-                            Öğrenci Bilgi Sistemine Hoşgeldiniz!
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
+    if (!isPagePermissionSuccess) {
         return (
             <UnauthorizedAccessPage user=""/>
         )
     }
+
+    return (
+        <div>
+            <SISTitle/>
+            <StudentNavbar/>
+            <div className="py-10 bg-sis-yellow rounded-3xl shadow-xl ml-80 mr-80 mt-56">
+                <div className="mb-2 select-none font-phenomenaRegular text-6xl text-center text-sis-white">
+                    <div>
+                        Merhaba <a className="font-phenomenaBold">{getStudentFullName()}</a>,
+                    </div>
+                    <div>
+                        Öğrenci Bilgi Sistemine Hoşgeldiniz!
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
