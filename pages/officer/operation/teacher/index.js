c3c19fd589287cb54fe27a26def2b3fd92facaa3
berinkrktt <73306985+berinkrktt@users.noreply.github.com>
2022-03-05 16:34:34 +0300

    Null List Transaction | SIS-121 (#43)
    * Department and Faculty List page Updated and Fixed
    
    * Lesson List page Updated and Fixed
    
    * Officer List page Updated and Fixed
    
    * Student List page Updated and Fixed
    
    * Teacher List page Updated and Fixed

--- pages/officer/operation/teacher/index.js
+++ pages/officer/operation/teacher/index.js
@@ -65,7 +65,7 @@ export default function TeacherList({isPagePermissionSuccess, teachers}) {
                     </button>
                 </div>
                 {(
-                    teachers !== null
+                    teachers.length !== 0
                         ?
                         <div className="flex flex-col">
                             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
