ac1216e87f34b88aa10d402feceae4f85814701c
berinkrktt <73306985+berinkrktt@users.noreply.github.com>
2022-03-05 16:02:34 +0300

    Teacher Role Transaction | SIS-84 (#39)
    * Teacher Navbar Adjusted For Role
    
    * Teacher Component Updated and Fixed
    
    * Teacher Navbar Updated
    
    * Teacher Component Updated and add cookies function.
    
    * Teacher Component Updated and fixed.
    
    * Teacher Component and Navbar Updated and fixed.
    
    * Teacher Component Updated and fixed.
    
    Co-authored-by: agitrubard <demiragitrubar@gmail.com>

--- public/components/navbar/teacher/teacher-component.js
+++ public/components/navbar/teacher/teacher-component.js
@@ -9,8 +9,10 @@ import {
     ViewGridIcon,
 } from '@heroicons/react/outline'
 import {ChevronDownIcon} from '@heroicons/react/solid'
+import {getTeacherRole} from "../../../storage/teacher";
 
-const solutions = [
+
+const lesson = [
     {
         name: 'Derslerim',
         description: '',
@@ -25,28 +27,31 @@ const solutions = [
     }
 ]
 
-const resources = [
+const otherTransactions = [
     {
         name: 'Staj İşlemleri',
         description: '',
         href: '/student/notes',
+        roles: ['ADVISOR', 'HEAD_OF_DEPARTMENT'],
         icon: BookmarkAltIcon,
     },
     {
         name: 'Mezuniyet İşlemleri',
         description: '',
         href: '/student/transcript',
+        roles: ['ADVISOR', 'HEAD_OF_DEPARTMENT'],
         icon: DocumentReportIcon,
     },
     {
         name: 'Ders Kayıt İşlemleri',
         description: '',
         href: '/student/transcript',
+        roles: ['ADVISOR', 'HEAD_OF_DEPARTMENT'],
         icon: CalendarIcon,
     },
 ]
 
-const schedules = [
+const programs = [
     {
         name: 'Ders Programı',
         description: "",
@@ -67,8 +72,10 @@ function classNames(...classes) {
 
 export default function TeacherComponent() {
 
+    const teacherRole = getTeacherRole();
+
     return (
-        <Popover.Group as="nav" className="hidden md:flex space-x-10 font-phenomenaBold">
+        <Popover.Group as="nav" className="select-none hidden md:flex space-x-10 font-phenomenaBold">
             <Popover className="relative">
                 {({open}) => (
                     <>
@@ -102,7 +109,7 @@ export default function TeacherComponent() {
                                 <div
                                     className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                                     <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                                        {solutions.map((item) => (
+                                        {lesson.map((item) => (
                                             <a
                                                 key={item.name}
                                                 href={item.href}
@@ -113,61 +120,6 @@ export default function TeacherComponent() {
                                                 <div className="ml-4">
                                                     <p className="font-phenomenaRegular text-xl text-gray-800">{item.name}</p>
                                                     <p className="mt-1 text-sm text-gray-500 font-phenomenaThin">{item.description}</p>
-                                                </div>
-                                            </a>
-                                        ))}
-                                    </div>
-                                </div>
-                            </Popover.Panel>
-                        </Transition>
-                    </>
-                )}
-            </Popover>
-
-            <Popover className="relative">
-                {({open}) => (
-                    <>
-                        <Popover.Button
-                            className={classNames(
-                                open ? 'text-sis-yellow' : 'text-gray-500',
-                                'group bg-white rounded-md inline-flex items-center text-xl hover:text-sis-yellow focus:outline-none'
-                            )}
-                        >
-                            <span>DİĞER İŞLEMLERİ</span>
-                            <ChevronDownIcon
-                                className={classNames(
-                                    open ? 'text-sis-yellow' : 'text-gray-500',
-                                    'ml-2 h-5 w-5 group-hover:text-sis-yellow'
-                                )}
-                                aria-hidden="true"
-                            />
-                        </Popover.Button>
-
-                        <Transition
-                            as={Fragment}
-                            enter="transition ease-out duration-200"
-                            enterFrom="opacity-0 translate-y-1"
-                            enterTo="opacity-100 translate-y-0"
-                            leave="transition ease-in duration-150"
-                            leaveFrom="opacity-100 translate-y-0"
-                            leaveTo="opacity-0 translate-y-1"
-                        >
-                            <Popover.Panel
-                                className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
-                                <div
-                                    className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                    <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                                        {resources.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                                            >
-                                                <item.icon className="flex-shrink-0 h-6 w-6 text-sis-yellow"
-                                                           aria-hidden="true"/>
-                                                <div className="ml-4">
-                                                    <p className="font-phenomenaRegular text-xl text-gray-800">{item.name}</p>
-                                                    <p className="mt-1 text-sm text-gray-500">{item.description}</p>
                                                 </div>
                                             </a>
                                         ))}
@@ -222,7 +174,7 @@ export default function TeacherComponent() {
                                 <div
                                     className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                                     <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                                        {schedules.map((item) => (
+                                        {programs.map((item) => (
                                             <a
                                                 key={item.name}
                                                 href={item.href}
@@ -243,6 +195,67 @@ export default function TeacherComponent() {
                     </>
                 )}
             </Popover>
+
+            {(
+                teacherRole === 'ADVISOR' || teacherRole === 'HEAD_OF_DEPARTMENT'
+                    ?
+                    <Popover className="relative">
+                        {({open}) => (
+                            <>
+                                <Popover.Button
+                                    className={classNames(
+                                        open ? 'text-sis-yellow' : 'text-gray-500',
+                                        'group bg-white rounded-md inline-flex items-center text-xl hover:text-sis-yellow focus:outline-none'
+                                    )}
+                                >
+                                    <span>DİĞER İŞLEMLER</span>
+                                    <ChevronDownIcon
+                                        className={classNames(
+                                            open ? 'text-sis-yellow' : 'text-gray-500',
+                                            'ml-2 h-5 w-5 group-hover:text-sis-yellow'
+                                        )}
+                                        aria-hidden="true"
+                                    />
+                                </Popover.Button>
+
+                                <Transition
+                                    as={Fragment}
+                                    enter="transition ease-out duration-200"
+                                    enterFrom="opacity-0 translate-y-1"
+                                    enterTo="opacity-100 translate-y-0"
+                                    leave="transition ease-in duration-150"
+                                    leaveFrom="opacity-100 translate-y-0"
+                                    leaveTo="opacity-0 translate-y-1"
+                                >
+                                    <Popover.Panel
+                                        className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
+                                        <div
+                                            className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                                                {otherTransactions.map((item) => (
+                                                    <a
+                                                        key={item.name}
+                                                        href={item.href}
+                                                        className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
+                                                    >
+                                                        <item.icon className="flex-shrink-0 h-6 w-6 text-sis-yellow"
+                                                                   aria-hidden="true"/>
+                                                        <div className="ml-4">
+                                                            <p className="font-phenomenaRegular text-xl text-gray-800">{item.name}</p>
+                                                            <p className="mt-1 text-sm text-gray-500">{item.description}</p>
+                                                        </div>
+                                                    </a>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    </Popover.Panel>
+                                </Transition>
+                            </>
+                        )}
+                    </Popover>
+                    :
+                  null
+            )}
 
         </Popover.Group>
     )
