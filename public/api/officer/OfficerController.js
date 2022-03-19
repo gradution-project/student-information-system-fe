d83c6619064576c73c1fd9851064187f3d3aafcb
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-03-19 09:41:23 +0300

    OfficerController Has Been Created for All Endpoints

--- public/api/officer/OfficerController.js
+++ public/api/officer/OfficerController.js
@@ -2,6 +2,15 @@
 const SIS_API_URL = process.env.NEXT_PUBLIC_SIS_API_URL;
 const SIS_FE_URL = process.env.NEXT_PUBLIC_SIS_FE_URL;
 
+
+const getAllOfficersByStatus = async (status) => {
+    const apiResult = await fetch(`${SIS_API_URL}/officer?status=${status}`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'GET'
+    });
+    return await apiResult.json();
+};
+
 const getOfficerDetailByOfficerId = async (officerId) => {
     const apiResult = await fetch(`${SIS_API_URL}/officer/${officerId}`, {
         headers: {'Content-Type': 'application/json'},
@@ -10,11 +19,55 @@ const getOfficerDetailByOfficerId = async (officerId) => {
     return await apiResult.json();
 };
 
-const updatePersonalInfo = async (operationUserId,
-                                  personalInfoResponse,
-                                  address, email, phoneNumber) => {
+const saveOfficer = async (operationUserId, academicInfo, personalInfo) => {
 
-    const apiResult = await fetch(`${SIS_API_URL}/officer/update/personal-info/${personalInfoResponse.officerId}`, {
+    const apiResult = await fetch(`${SIS_API_URL}/officer/save`, {
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId,
+                feUrl: SIS_FE_URL
+            },
+            academicInfoRequest: {
+                facultyId: academicInfo.facultyId,
+                phoneNumber: academicInfo.academicPhoneNumber
+            },
+            personalInfoRequest: {
+                address: personalInfo.address,
+                birthday: personalInfo.birthday,
+                email: personalInfo.email,
+                name: personalInfo.name,
+                phoneNumber: personalInfo.personalPhoneNumber,
+                surname: personalInfo.surname,
+                tcNo: personalInfo.tcNo
+            }
+        }),
+        headers: {'Content-Type': 'application/json'},
+        method: 'POST'
+    });
+    return await apiResult.json();
+};
+
+const updateOfficerAcademicInfo = async (operationUserId, officerId, academicInfo) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/officer/update/academic-info/${officerId}`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PUT',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            academicInfoRequest: {
+                facultyId: academicInfo.facultyId,
+                phoneNumber: academicInfo.phoneNumber,
+            }
+        }),
+    });
+    return await apiResult.json();
+};
+
+const updateOfficerPersonalInfo = async (operationUserId, officerId, personalInfo) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/officer/update/personal-info/${officerId}`, {
         headers: {'Content-Type': 'application/json'},
         method: 'PUT',
         body: JSON.stringify({
@@ -22,14 +75,59 @@ const updatePersonalInfo = async (operationUserId,
                 userId: operationUserId
             },
             personalInfoRequest: {
-                address: address,
-                birthday: personalInfoResponse.birthday,
-                email: email,
-                name: personalInfoResponse.name,
-                phoneNumber: phoneNumber,
-                surname: personalInfoResponse.surname,
-                tcNo:personalInfoResponse.tcNo
+                address: personalInfo.address,
+                birthday: personalInfo.birthday,
+                email: personalInfo.email,
+                name: personalInfo.name,
+                phoneNumber: personalInfo.phoneNumber,
+                surname: personalInfo.surname,
+                tcNo:personalInfo.tcNo
             }
+        }),
+    });
+    return await apiResult.json();
+};
+
+const activateOfficer = async (operationUserId, officerId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/officer/activate`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PATCH',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            officerId: officerId
+        }),
+    });
+    return await apiResult.json();
+};
+
+const passivateOfficer = async (operationUserId, officerId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/officer/passivate`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PATCH',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            officerId: officerId
+        }),
+    });
+    return await apiResult.json();
+};
+
+const deleteOfficer = async (operationUserId, officerId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/officer/delete`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'DELETE',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            officerId: officerId
         }),
     });
     return await apiResult.json();
@@ -37,8 +135,14 @@ const updatePersonalInfo = async (operationUserId,
 
 
 const OfficerController = {
+    getAllOfficersByStatus,
     getOfficerDetailByOfficerId,
-    updatePersonalInfo
+    saveOfficer,
+    updateOfficerAcademicInfo,
+    updateOfficerPersonalInfo,
+    activateOfficer,
+    passivateOfficer,
+    deleteOfficer
 };
 
 export default OfficerController;
