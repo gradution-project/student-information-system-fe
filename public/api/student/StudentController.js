975784a9bd773479df92bcca8b8a47cd512f8cab
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-03-19 09:40:45 +0300

    StudentController Has Been Created for All Endpoints

--- public/api/student/StudentController.js
+++ public/api/student/StudentController.js
@@ -2,6 +2,14 @@
 const SIS_API_URL = process.env.NEXT_PUBLIC_SIS_API_URL;
 const SIS_FE_URL = process.env.NEXT_PUBLIC_SIS_FE_URL;
 
+const getAllStudentsByStatus = async (status) => {
+    const apiResult = await fetch(`${SIS_API_URL}/student?status=${status}`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'GET'
+    });
+    return await apiResult.json();
+};
+
 const getStudentDetailByStudentId = async (studentId) => {
     const apiResult = await fetch(`${SIS_API_URL}/student/${studentId}`, {
         headers: {'Content-Type': 'application/json'},
@@ -10,9 +18,147 @@ const getStudentDetailByStudentId = async (studentId) => {
     return await apiResult.json();
 };
 
+const saveStudent = async (operationUserId, academicInfo, personalInfo) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/save`, {
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId,
+                feUrl: SIS_FE_URL
+            },
+            academicInfoRequest: {
+                degree: academicInfo.degree,
+                departmentId: academicInfo.departmentId,
+                classLevel: academicInfo.classLevel,
+            },
+            personalInfoRequest: {
+                address: personalInfo.address,
+                birthday: personalInfo.birthday,
+                email: personalInfo.email,
+                name: personalInfo.name,
+                phoneNumber: personalInfo.phoneNumber,
+                surname: personalInfo.surname,
+                tcNo: personalInfo.tcNo
+            }
+        }),
+        headers: {'Content-Type': 'application/json'},
+        method: 'POST'
+    });
+    return await apiResult.json();
+};
+
+const updateStudentAcademicInfo = async (operationUserId, studentId, academicInfo) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/update/academic-info/${studentId}`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PUT',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            academicInfoRequest: {
+                classLevel: academicInfo.classLevel,
+                degree: academicInfo.degree,
+                departmentId: academicInfo.departmentId
+            }
+        }),
+    });
+    return await apiResult.json();
+};
+
+const updateStudentPersonalInfo = async (operationUserId, studentId, personalInfo) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/update/personal-info/${studentId}`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PUT',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            personalInfoRequest: {
+                address: personalInfo.address,
+                birthday: personalInfo.birthday,
+                email: personalInfo.email,
+                name: personalInfo.name,
+                phoneNumber: personalInfo.phoneNumber,
+                surname: personalInfo.surname,
+                tcNo:personalInfo.tcNo
+            }
+        }),
+    });
+    return await apiResult.json();
+};
+
+const activateStudent = async (operationUserId, studentId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/activate`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PATCH',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            studentId: studentId
+        }),
+    });
+    return await apiResult.json();
+};
+
+const passivateStudent = async (operationUserId, studentId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/passivate`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PATCH',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            studentId: studentId
+        }),
+    });
+    return await apiResult.json();
+};
+
+const deleteStudent = async (operationUserId, studentId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/delete`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'DELETE',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            studentId: studentId
+        }),
+    });
+    return await apiResult.json();
+};
+
+const graduateStudent = async (operationUserId, studentId) => {
+
+    const apiResult = await fetch(`${SIS_API_URL}/student/graduate`, {
+        headers: {'Content-Type': 'application/json'},
+        method: 'PATCH',
+        body: JSON.stringify({
+            operationInfoRequest: {
+                userId: operationUserId
+            },
+            studentId: studentId
+        }),
+    });
+    return await apiResult.json();
+};
 
 const StudentController = {
-    getStudentDetailByStudentId
+    getAllStudentsByStatus,
+    getStudentDetailByStudentId,
+    saveStudent,
+    updateStudentAcademicInfo,
+    updateStudentPersonalInfo,
+    activateStudent,
+    passivateStudent,
+    deleteStudent,
+    graduateStudent
 };
 
 export default StudentController;
