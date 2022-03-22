da1d1a5d875eb761a96f56cab76815993c97b8eb
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-03-22 13:28:41 +0300

    Feature Toggle Name Constant File Has Been Created

---
+++ public/constants/university/FeatureToggleName.js
@@ -0,0 +1,21 @@
+const getAll = [
+    {
+        enum: 'LESSON_REGISTRATION_OPERATIONS',
+        tr: 'Ders Kayıt İşlemleri'
+    },
+    {
+        enum: 'NOTE_OPERATIONS',
+        tr: 'Not İşlemleri'
+    }
+]
+
+const LESSON_REGISTRATION_OPERATIONS = 'LESSON_REGISTRATION_OPERATIONS';
+const NOTE_OPERATIONS = 'NOTE_OPERATIONS';
+
+const FeatureToggleName = {
+    getAll,
+    LESSON_REGISTRATION_OPERATIONS,
+    NOTE_OPERATIONS
+};
+
+export default FeatureToggleName;
