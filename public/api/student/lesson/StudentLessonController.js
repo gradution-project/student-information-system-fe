85eade1b85e0dcc9a6fbb1d1cf0e3200b78de891
Agit Rubar Demir <demiragitrubar@gmail.com>
2022-05-06 21:39:56 +0300

    StudentLessonController Endpoints Have Been Fixed and Updated

--- public/api/student/lesson/StudentLessonController.js
+++ public/api/student/lesson/StudentLessonController.js
@@ -2,7 +2,7 @@
 const SIS_API_URL = process.env.NEXT_PUBLIC_SIS_API_URL;
 
 const getAllStudentLessonsByStudentId = async (studentId) => {
-    const apiResult = await fetch(`${SIS_API_URL}/student/lesson/get/${studentId}`, {
+    const apiResult = await fetch(`${SIS_API_URL}/student/lessons/by/student/${studentId}`, {
         headers: {'Content-Type': 'application/json'},
         method: 'GET'
     });
